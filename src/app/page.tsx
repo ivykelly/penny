681e9794e5ed556b24b89406e562ca6c09f95d18
Maya Novichenok <mayanovichenok@Mayas-MacBook-Pro-2.local>
2025-01-18 15:38:03 -0500

    home page 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 const learningPaths = [
-  { id: 1, title: 'Basics', icon: '/icons/basics.png', progress: 100 },
-  { id: 2, title: 'Stock Market', icon: '/icons/stocks.png', progress: 50 },
-  { id: 3, title: 'Retirement', icon: '/icons/retirement.png', progress: 0 },
-  { id: 4, title: 'Crypto', icon: '/icons/crypto.png', progress: 0 },
-  { id: 5, title: 'Real Estate', icon: '/icons/real-estate.png', progress: 0 },
-  { id: 6, title: 'Budgeting', icon: '/icons/budgeting.png', progress: 0 },
-  { id: 7, title: 'Taxes', icon: '/icons/taxes.png', progress: 0 },
-  { id: 8, title: 'DebtManagement', icon: '/icons/debt.png', progress: 0 },
-  { id: 9, title: 'Insurance', icon: '/icons/insurance.png', progress: 0 },
+  { id: 1, title: 'Basics', icon: 'basics.png', progress: 100 },
+  { id: 2, title: 'Bonds', icon: 'bonds.png', progress: 59 },
+  { id: 3, title: 'Stocks', icon: 'stocks.png', progress: 0 },
+  { id: 4, title: 'Mutual Funds', icon: 'crypto.png', progress: 0 },
+  { id: 5, title: 'Dividends', icon: 'real-estate.png', progress: 0 },
+  { id: 6, title: 'Strategy', icon: 'budgeting.png', progress: 0 },
+  { id: 7, title: 'Savings', icon: 'taxes.png', progress: 0 },
+  { id: 8, title: 'Risk', icon: 'debt.png', progress: 0 },
+  { id: 9, title: 'Portfolio', icon: 'insurance.png', progress: 0 },
 ];
 
 export default function Home() {
@@ -27,8 +27,8 @@ export default function Home() {
         {/* First Row - Single Item */}
         <div className="w-full max-w-2xl flex justify-center">
           <div className="flex flex-col space-y-2 w-[calc(50%-32px)]">
-            <span className="text-sm font-bold text-pink-900 text-center">
-              {learningPaths[0].title}
+          <span className="text-1.8xl font-bold text-pink-925 text-center">
+          {learningPaths[0].title}
             </span>
             <button
               onClick={() => router.push(`/learn/${learningPaths[0].id}`)}
@@ -57,7 +57,7 @@ export default function Home() {
         <div className="grid grid-cols-2 gap-x-16 gap-y-10 w-full max-w-2xl">
           {learningPaths.slice(1).map((path) => (
             <div key={path.id} className="flex flex-col space-y-2">
-              <span className="text-sm font-bold text-pink-900 text-center">
+              <span className="text-1.8xl font-bold text-pink-925 text-center">
                 {path.title}
               </span>
               <button
