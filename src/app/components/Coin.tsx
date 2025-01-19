8ccb1505e3eadc6df589cb20b0d372f08c44865a
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-01-18 22:44:48 -0500

    truncate floating pts

--- src/app/components/Coin.tsx
+++ src/app/components/Coin.tsx
@@ -7,10 +7,12 @@ import { useCoin } from "../contexts/CoinContext";
 export default function Coin() {
     const { coins } = useCoin(); // Get the user's coins
 
+    const coinsTruncated = Math.floor(coins);
+
     return (
         <div className="flex flex-col items-center">
             <Image src="/pennyWithCoin.png" alt="" width={644} height={686} className="max-w-32" />
-            <p className="-mt-3 text-xl font-extrabold text-amber-400">{coins}p</p>
+            <p className="-mt-3 text-xl font-extrabold text-amber-400">{coinsTruncated}p</p>
         </div>
     );
 }
